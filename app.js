067982887c99ffefb6de993343144e98bc5859ee
ionware <ionwarez@gmail.com>
2019-05-31 12:25:50 +0100

    added server.js to initilize the server

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const MongoStore = require('connect-mongo')(session);
 const flash = require('connect-flash');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
+const expressValidator = require('express-validator');
 const app = express();
 
 // Take request payloads into body.
-app.use(bodyParser({ extended: false }));
 app.use(bodyParser.json());
+app.use(bodyParser({ extended: true }));
 
 // Let us have support for session.
 app.use(session({
@@ -19,3 +21,12 @@ app.use(session({
 }));
 // Flashing of messages to the session.
 app.use(flash());
+// Awesome validation on our requests.
+app.use(expressValidator());
+// Serve static assets from the public folder.
+app.use(express.static(path.join(__dirname, 'public')));
+// Configure our view engine to be Pug.
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '.env')});
+const app = require('./app');
+
+mongoose.connect(process.env.DB_CONNECTION, {
+    useNewUrlParser: true
+});
+
+mongoose.connection.on('error', (error) => {
+    console.error(error.message());
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Database connected Successfully.');
+});
+
+// Light Up the Server!!
+app.listen(process.env.APP_PORT, () => {
+    console.log(`Server started on PORT ---> ${process.env.APP_PORT}`);
+});
